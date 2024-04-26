8df1016258708a6acb97f4ac8c069653e47df7e1
ImanGholami <imangholamiimi@gmail.com>
2024-04-26 21:05:49 +0330

    :zap: | UPDATE | src | settings | add email configs

--- src/settings.py
+++ src/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Email_sender',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'example@gmail.com' # Email Sender
+EMAIL_HOST_PASSWORD = 'Password App' # A 16-character password
