adb6211ce85e18ee7a5f8cdeda25f4c436451b0f
ImanGholami <imangholamiimi@gmail.com>
2024-04-26 21:10:21 +0330

    :tada: | ADD | VIEWS | send_email_view

--- Email_sender/views.py
+++ Email_sender/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponse
 
-# Create your views here.
+def send_email_view(request):
+    send_mail(
+        'Subject here',       # Subject of the email
+        'Here is the message.',  # Message of the email
+        'example@gmail.com',   # Sender's email address
+        ['to@example.com'],   # List of recipient(s)
+        fail_silently=False,  # Whether to raise an exception if sending fails
+    )
+    return HttpResponse('Email sent successfully!')
